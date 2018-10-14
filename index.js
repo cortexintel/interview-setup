a0fbcae1287e792d564c241516a24e1193b3435e
Colby McBride <colbym84@gmail.com>
2018-10-14 10:08:49 -0400

    Setup and connected to slack

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-var http = require('http');
-var axios = require('axios');
-
-var bot_token = process.env.SLACK_BOT_TOKEN;
-var darksky_token = process.env.DARKSKY_TOKEN;
+/**
+  Setup variables
+**/
+const bot_token = '';
+const darksky_token = '';
 
 if (!bot_token) {
-  console.log("Missing SLACK_BOT_TOKEN environement variable ❌")
-  process.exit(1)
+  console.log("Missing SLACK_BOT_TOKEN environment variable ❌");
+  process.exit(1);
 }
 
 if (!darksky_token) {
-  console.log("Missing DARKSKY_TOKEN environement variable ❌")
-  process.exit(1)
+  console.log("Missing DARKSKY_TOKEN environment variable ❌");
+  process.exit(1);
 }
 
-var rtm = new RtmClient(bot_token);
+const darkSkyUrl = `https://api.darksky.net/forecast/${darksky_token}/40.7128,-74.0059`;
+const channel = '';
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log("Slack Successfully Authenticated ✅");
-  
-  axios
-    .get("https://api.darksky.net/forecast/" +  darksky_token + "/40.7128,-74.0059")
-    .then(function (response) {
-      console.log('Darksky Successfully Authenticated ✅')
-      process.exit()
-    })
-    .catch(function (error) {
-      console.log('Error accessing darksky ❌')
-      console.log(error)
-      process.exit(1)
-    })
-});
+const rtm = new RtmClient(bot_token);
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, rtmStartData => console.log('slackbot is running..'));
+
+rtm.on(RTM_EVENTS.MESSAGE, message => {
+  console.log(message);
+})
 
 rtm.start();
