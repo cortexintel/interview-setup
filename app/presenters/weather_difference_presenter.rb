7cf137978f5049a9eaeee4b166057b2c22c3a69f
tronroy <tbconroy@gmail.com>
2018-11-17 12:56:00 -0500

    Round delta values in difference presenter, fix syntax error

--- app/presenters/weather_difference_presenter.rb
+++ app/presenters/weather_difference_presenter.rb
@@ -7,10 +7,10 @@ class WeatherDifferencePresenter
     <<~MESSAGE
     ALERT: Significant Change In Weather From Yesterday
 
-    Change in temperature high: #{weather_difference.temperature_high}˚F
-    Change in temperature low: #{weather_difference.temperature_low}˚F
-    Change in humidity: #{weather_difference.humidity}%
-    Change in precipitation: #{weather_difference.precip_probability}%
+    Change in temperature high: #{weather_difference.temperature_high.round(2)}˚F
+    Change in temperature low: #{weather_difference.temperature_low.round(2)}˚F
+    Change in humidity: #{weather_difference.humidity.round(2)}%
+    Change in precipitation: #{weather_difference.precip_probability.round(2)}%
     MESSAGE
   end
 
--- lib/tasks/differentiate_weather.rake
+++ lib/tasks/differentiate_weather.rake
@@ -2,7 +2,7 @@ desc "Differentiates weather of today from yesterday and posts results to Slack"
 task differentiate_weather: :environment  do
   weather_differences = DifferentiateWeather.call
   if weather_differences.significant?
-    message = WeatherDifferencePresenter.new(weather_difference).call
+    message = WeatherDifferencePresenter.new(weather_differences).call
     PostSlackMessage.call(message, '#general')
   end
 end
